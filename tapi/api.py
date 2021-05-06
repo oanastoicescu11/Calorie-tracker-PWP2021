07226d2092adb95e47ca1d7d7f70e7ff50f4f9b2
Oana Stoicescu <oanastoicescu11@gmail.com>
2021-05-06 17:24:43 +0300

    entrypoint+ apiary redirect

--- tapi/api.py
+++ tapi/api.py
@@ -3,8 +3,8 @@
 # https://flask-restful.readthedocs.io/en/latest/intermediate-usage.html#use-with-blueprints
 
 # This is used in the __init__ when creating the Flask instance
-
-from flask import Blueprint
+import json
+from flask import Blueprint, Response, redirect
 from flask_restful import Api
 from tapi.constants import *
 
@@ -16,12 +16,25 @@ from tapi.resources.meal import MealItem
 from tapi.resources.mealrecord import MealRecordItem
 from tapi.resources.mealportion import MealPortionItem
 from tapi.resources.portion import PortionItem
+from tapi.utils import CalorieBuilder, add_mason_response_header, add_calorie_namespace
+
 
 api.add_resource(PersonItem, ROUTE_PERSON, ROUTE_PERSON_COLLECTION)
 api.add_resource(MealItem, ROUTE_MEAL, ROUTE_MEAL_COLLECTION)
 api.add_resource(MealRecordItem, ROUTE_MEALRECORD, ROUTE_MEALRECORD_COLLECTION)
 api.add_resource(MealPortionItem, ROUTE_MEALPORTION)
 api.add_resource(PortionItem, ROUTE_PORTION, ROUTE_PORTION_COLLECTION)
+
+
+# Route for entry point
+@api_blueprint.route('/')
+def entrypoint():
+    resp = CalorieBuilder()
+    resp.add_control(NS + ':persons-all', api.url_for(PersonItem, handle=None))
+    resp.add_control(NS + ':meals-all', api.url_for(MealItem, handle=None))
+    resp.add_control(NS + ':portions-all', api.url_for(PortionItem, handle=None))
+    add_calorie_namespace(resp)
+    return Response(json.dumps(resp), 200, headers=add_mason_response_header())
 
 
 # Route for MealRecords for person
@@ -34,3 +47,10 @@ def meals_for_person(handle):
 @api_blueprint.route('/meals/<handle>/mealportions/', methods=['POST'])
 def mealportions_for_meal(handle):
     return MealPortionItem.post(handle)
+
+
+APIARY_URL = "https://pwp2021calorie.docs.apiary.io/#reference/"
+
+@api_blueprint.route('/link-relations/')
+def redirect_to_apiary_link_rels():
+    return redirect(APIARY_URL + "link-relations")
